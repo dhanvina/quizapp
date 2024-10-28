25376a8aaede0d9dddfa01eead1cc5e4c1f81acf
N Dhanvina <47035051+dhanvina@users.noreply.github.com>
2024-10-29 00:00:47 +0530

    Update main.dart
    trying to comment firebase initialization from main branch

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+    // options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(MyApp());
 }
 
