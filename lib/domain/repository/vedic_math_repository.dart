7aa935556b26497334412d987fecc4bfb07d2fe7
unknown <peckdoor@gmail.com>
2024-11-19 20:59:08 +0530

    [FIX]: Optimized json by removing question_paper and making it individual structure

--- lib/domain/repository/vedic_math_repository.dart
+++
@@ -1,5 +0,0 @@
-// import '../entities/vedic_math_entity.dart';
-//
-// abstract class VedicMathRepositoryInterface {
-//   Future<List<VedicMathPracticePaperEntity>> getVedicQuestionPapers();
-// }
